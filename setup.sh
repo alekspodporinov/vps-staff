4bc0cf9a2e962880d4c5cdfa04de78c1dfe7c775
alekspodporinov <oleksandr.podporinov@evolved-ideas.com>
2025-12-28 20:15:11 +0200

    Update download links in setup script to point to the correct repository

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 
-#wget "https://dev.azure.com/alekspodpdev-open/Sandbox/_apis/git/repositories/bash-scripts/items?path=/setup/setup.sh&version=main&download=true" -O setup.sh && sudo chmod +x setup>
+#wget "https://github.com/alekspodporinov/vps-staff/blob/main/setup.sh" -O setup.sh && sudo chmod +x setup>
 
 if [ "$(id -u)" -ne 0 ]; then
  echo "This script requires root privileges. Please run as root or use sudo."
@@ -102,7 +102,7 @@ case "$SSH_OPTION" in
  2)
    echo "Downloading the default SSH public key..."
    mkdir -p /home/"$CURRENT_USER"/.ssh
-   wget "https://dev.azure.com/alekspodpdev-open/Sandbox/_apis/git/repositories/bash-scripts/items?path=/setup/devap_pbk&version=main&download=true" -O /home/"$CURRENT_USER"/.ssh/a>
+   wget "https://github.com/alekspodporinov/vps-staff/blob/main/devap_pbk" -O /home/"$CURRENT_USER"/.ssh/a>
    if [ $? -eq 0 ]; then
      chmod 700 /home/"$CURRENT_USER"/.ssh
      chmod 600 /home/"$CURRENT_USER"/.ssh/authorized_keys
